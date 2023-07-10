0d4bbe53eae38e8a700cd595657d1241a821b8bf
Administrator <deuanchambers94@gmail.com>
2023-07-09 23:42:36 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+
+let count=0
+
+// update(inputV)
+function update(){
+ 
+let inputV=document.getElementById("input").value
+
+    for(let i =0; i<inputV.length-1;i++){
+    
+        if(inputV[i]==="a"||inputV[i]==="e"||inputV[i]==="i"||inputV[i]==="o"||inputV[i]==="u"||inputV[i]==="y" ){
+        console.log(count++)
+
+        }
+        
+        
+        
+    }
+
+   
+}
+
+
